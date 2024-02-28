8e0e1ce3c47adebabbebe8036d9506386fed64ae
Quim <joaquimpujol@gmail.com>
2024-02-28 17:01:24 +0100

    Inicio Sección 5

---
+++ src/app/dbz/pages/main-page.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-dbz-main-page',
+    templateUrl: 'name.component.html'
+})
+
+export class NameComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
